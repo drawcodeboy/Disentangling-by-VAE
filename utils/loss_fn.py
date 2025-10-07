aa9750039ea4be133f424cec7046df58591799e3
drawcodeboy <20rnjsekdns@naver.com>
2025-10-08 00:47:28 +0900

    [Update] new perspective not bernouli

--- utils/loss_fn.py
+++ utils/loss_fn.py
@@ -10,15 +10,19 @@ class ELBO(nn.Module):
         self.beta = beta # if beta == 1.0 -> VanilaVAE, beta > 1.0 -> BetaVAE 
     
     def forward(self, x_prime, x, mu, log_var):
+        # Reconstruction Loss 관련 Line 429-433 참고
+        # https://github.com/YannDubs/disentangling-vae/blob/master/disvae/models/losses.py#L394
+
+        # dim별로는 summation, batch별로는 mean을 구하는 구조
+
         # 1) Regularization
         first_term = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp(), dim=1).mean()
         
         # 2) Reconstruction
-        # second_term = F.mse_loss(x_prime, x)
         n = x.size(0)
-        second_term = F.binary_cross_entropy_with_logits(x_prime, x, reduction='sum') / n
-        # second_term = F.l1_loss()
-        
+        # second_term = F.mse_loss(x_prime * 255., x * 255., reduction='sum') / (255. * n)
+        second_term = F.mse_loss(x_prime, x, reduction='sum') / n
+
         minus_elbo = self.beta * first_term + second_term
         
         return minus_elbo, first_term, second_term
