9761ef02d701e2903b2bffec9251db3d25aa0ed8
drawcodeboy <20rnjsekdns@naver.com>
2025-10-08 00:46:59 +0900

    [Remove] legacy

--- utils/engine.py
+++ utils/engine.py
@@ -25,22 +25,6 @@ def train_one_epoch(model, dataloader, loss_fn, optimizer, scheduler, task_cfg, 
             total_loss.append(loss.item())
             loss.backward()
             optimizer.step()
-        
-        elif task_cfg['object'] == 'train_vovae':
-            optimizer.zero_grad()
-            
-            x, label = data
-            x = x.to(device)           
-            label = label.to(device)
-            
-            x_prime, z, mu, log_var, orth_loss = model(x)
-            loss, _, __ = loss_fn(x_prime, x, mu, log_var)
-            
-            loss += orth_loss
-            
-            total_loss.append(loss.item())
-            loss.backward()
-            optimizer.step()
 
         else:
             raise Exception("Check your task_cfg['object'] configuration")
@@ -64,15 +48,6 @@ def validate(model, dataloader, loss_fn, scheduler, task_cfg, device):
             x_prime, z, mu, log_var = model(x)
             loss, _, __ = loss_fn(x_prime, x, mu, log_var)
         
-        elif task_cfg['object'] == 'train_vovae':
-            x, label = data
-            x = x.to(device)           
-            label = label.to(device)
-            
-            x_prime, z, mu, log_var, orth_loss = model(x)
-            loss, _, __ = loss_fn(x_prime, x, mu, log_var)
-            loss += orth_loss
-        
         else:
             raise Exception("Check your task_cfg['object'] configuration")
         
@@ -90,7 +65,7 @@ def evaluate(model, dataloader, task_cfg, device):
     model.eval()
     
     # 랜덤 시드 필요 없다. DCI 논문에서 보면, Q(x)에서 리턴하는 mean을 사용한다고 되어있다.
-    # 나 역시 이에 동의한다. z로 샘플링하면서 무작위성을 주면, 그게 유효하다고 보기에는 적합하지 않다.
+    # 나 역시 이에 동의한다. z로 샘플링하면서 무작위성을 주면, 그게 평가에 유효하다고 보기에는 적합하지 않다.
     # random_seed = 10
     # random.seed(random_seed)
     # np.random.seed(random_seed)
@@ -106,18 +81,6 @@ def evaluate(model, dataloader, task_cfg, device):
             label = label.to(device)
 
             x_prime, z, mu, log_var = model(x)
-            
-            total_x.append(x.cpu().numpy())
-            total_x_prime.append(x_prime.cpu().numpy())
-            total_codes.append(mu.cpu().numpy()) # z 대신 mu
-            total_factors.append(label.cpu().numpy())
-        
-        elif task_cfg['object'] == 'test_vovae':
-            x, label = data
-            x = x.to(device)           
-            label = label.to(device)
-
-            x_prime, z, mu, log_var, orth_loss = model(x)
             
             total_x.append(x.cpu().numpy())
             total_x_prime.append(x_prime.cpu().numpy())
